e62588493d7d5df1d4037b9c7150a758a4af63f9
miaoguangfa <0x4d4746h@gmail.com>
2016-01-13 11:15:34 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://github.com/0x4d4746h/libimobiledevice.git
-cp ./libimobiledevice/bin/* /usr/local/bin/
-cp ./libimobiledevice/local_bin/* /usr/local/bin/
-cp ./libimobiledevice/local_lib/* /usr/local/lib/
+sudo cp ./libimobiledevice/bin/* /usr/local/bin/
+sudo cp ./libimobiledevice/local_bin/* /usr/local/bin/
+sudo cp ./libimobiledevice/local_lib/* /usr/local/lib/
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install openssl libxml2 p7zip --force-bottle
 rm -rf ./libimobiledevice
